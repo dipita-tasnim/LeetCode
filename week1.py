f59ea2b55c762323ab75784c9f4962c502f61c99
dipita-tasnim <tasnim.dipita@gmail.com>
2026-07-06 13:59:55 +0600

    Added reverse_string, merge_sorted_array

--- week1.py
+++ week1.py
@@ -239,3 +239,99 @@ def profit(arr):
         elif price - min_price > max_profit:
             max_profit = (price - min_price)      
     return max_profit   
+
+#------------------------------------------------------------------------------------------------------------------------------------------
+
+# Reverse String
+
+# Write a function that reverses a string. The input string is given as an array of characters s.
+# You must do this by modifying the input array in-place with O(1) extra memory.
+
+# Example 1:
+# Input: s = ["h","e","l","l","o"]
+# Output: ["o","l","l","e","h"]
+
+# Example 2:
+# Input: s = ["H","a","n","n","a","h"]
+# Output: ["h","a","n","n","a","H"]
+
+# O(1) --1 array--in place change
+
+def reverse_string(arr):
+    left = 0
+    right = len(arr)-1
+
+    while left < right:
+        temp = arr[left]
+        arr[left] = arr[right]
+        arr[right] = temp
+
+        left += 1
+        right -= 1
+    return arr 
+
+print(reverse_string(["h","e","l","l","o"]))  
+
+# or--O(n) ---created a new array makes 2 array--extra n space
+
+def reverse_string(arr):
+    reverse_arr = []
+    for i in range(len(arr)-1,-1,-1):
+        reverse_arr.append(arr[i])
+    return reverse_arr
+
+print(reverse_string(["h","e","l","l","o"]))
+
+#------------------------------------------------------------------------------------------------------------------------------------------------
+
+# Merge Sorted Array
+
+# You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+# Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+# The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+
+# Example 1:
+# Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+# Output: [1,2,2,3,5,6]
+# Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
+# The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
+
+# Example 2:
+# Input: nums1 = [1], m = 1, nums2 = [], n = 0
+# Output: [1]
+# Explanation: The arrays we are merging are [1] and [].
+# The result of the merge is [1].
+
+# Example 3:
+# Input: nums1 = [0], m = 0, nums2 = [1], n = 1
+# Output: [1]
+# Explanation: The arrays we are merging are [] and [1].
+# The result of the merge is [1].
+# Note that because m = 0, there are no elements in nums1. The 0 is only there to ensure the merge result can fit in nums1.
+
+def merge_sorted_array(nums1, m, nums2, n):
+    for i in range(n):
+        nums1[m+i] = nums2[i]
+    nums1.sort()
+    return nums1    
+
+print(merge_sorted_array([1,2,3,0,0,0], 3, [2,5,6], 3))
+
+
+# or---3 pointer (for learning purpose)
+
+def merge_sorted_array(nums1, m, nums2, n):
+    p1 = m - 1      # last position in real nums1
+    p2 = n - 1      # last position in nums2
+    p = m + n - 1   # last position in resulted nums1
+
+    while p2 >= 0: # while nums2 still has element
+        if p1 >= 0 and nums1[p1] > nums2[p2]: 
+            nums1[p] = nums1[p1]   # bigger one should be at towards the last
+            p1 -= 1                # decreasing the last position, going towards left
+        else:
+            nums1[p] = nums2[p2]
+            p2 -= 1
+        p -= 1
+    return nums1    
+print(merge_sorted_array([1,2,3,0,0,0], 3, [2,5,6], 3))   
