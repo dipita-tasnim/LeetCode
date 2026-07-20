0c38274bff0a36230094583d4546fa3342caf05d
dipita-tasnim <tasnim.dipita@gmail.com>
2026-07-20 22:24:55 +0600

    Added permutation in string

--- week2 (Two Pointers & Sliding Window).py
+++ week2 (Two Pointers & Sliding Window).py
@@ -414,3 +414,69 @@ def valid_palindrome(s):
     return True
 
 print(valid_palindrome("abca"))
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+# Permutation in String
+
+# Given two strings s1 and s2, return true if s2 contains a permutation of s1, or false otherwise.
+# In other words, return true if one of s1's permutations is the substring of s2.
+
+# Example 1:
+# Input: s1 = "ab", s2 = "eidbaooo"
+# Output: true
+# Explanation: s2 contains one permutation of s1 ("ba").
+
+# Example 2:
+# Input: s1 = "ab", s2 = "eidboaoo"
+# Output: false
+
+# s1 = "ab" (length 2)  →  window is ALWAYS size 2
+# s2 = e i d b a o o o
+#      [_]                 window at position 0
+#        [_]               slide right by 1
+#          [_]             slide again...
+
+
+def checkInclusion(s1, s2):
+    if len(s1) > len(s2):        # ← ADD THIS
+        return False
+    # count characters in s1 (target)
+    need = {}
+    for i in s1:
+        if i in need:
+            need[i] += 1
+        else:
+            need[i] = 1
+
+    # count characters in the first window (every window len = s1 len) of s2
+    window = {}
+    for i in range(len(s1)):
+        if s2[i] in window:
+            window[s2[i]] += 1
+        else:
+            window[s2[i]] = 1 
+        
+    if window == need:   # first window alreday matches
+        return True
+    
+    # slide the window one step at a time
+    for i in range(len(s1), len(s2)):
+        if s2[i] in window:
+            window[s2[i]] += 1
+        else:
+            window[s2[i]] = 1 
+
+        left_char = s2[i - len(s1)]
+        window[left_char] -= 1
+
+        if window[left_char] == 0:
+            del window[left_char] # remove zero counts so that == comparison works [both key, val deleted]   
+
+        if window == need:
+            return True
+    return False    
+
+print(checkInclusion("ab", "eidboaoo"))
+
+ 
