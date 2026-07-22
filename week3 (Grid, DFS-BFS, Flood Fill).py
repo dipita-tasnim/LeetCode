2396951e61993d28dc0bf70d806c99e967baf080
dipita-tasnim <tasnim.dipita@gmail.com>
2026-07-22 15:16:19 +0600

    Added Number of Islands

--- week3 (Grid, DFS-BFS, Flood Fill).py
+++ week3 (Grid, DFS-BFS, Flood Fill).py
@@ -113,3 +113,69 @@ def floodFill(image, sr, sc, color):
 
     return image
     
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+# Number of Islands
+
+# Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
+# An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+
+# Example 1:
+# Input: grid = [
+#   ["1","1","1","1","0"],
+#   ["1","1","0","1","0"],
+#   ["1","1","0","0","0"],
+#   ["0","0","0","0","0"]
+# ]
+# Output: 1
+# 🟩🟩🟩🟩🟦
+# 🟩🟩🟦🟩🟦
+# 🟩🟩🟦🟦🟦
+# 🟦🟦🟦🟦🟦
+
+# Example 2:
+# Input: grid = [
+#   ["1","1","0","0","0"],
+#   ["1","1","0","0","0"],
+#   ["0","0","1","0","0"],
+#   ["0","0","0","1","1"]
+# ]
+# Output: 3
+# 🟩🟩🟦🟦🟦   ← group A
+# 🟩🟩🟦🟦🟦   ← group A
+# 🟦🟦🟩🟦🟦   ← group B (alone)
+# 🟦🟦🟦🟩🟩   ← group C
+
+
+def numIsIland(grid):
+    count = 0
+    rows = len(grid)
+    cols = len(grid[0])
+
+    def sink(r, c):
+        if r < 0 or r > rows or c < 0 or c > cols:
+            return
+
+        if grid[r][c] != "1":   # water or already sunk
+            return
+
+        grid[r][c] = "0"        # sink it
+
+        sink(r + 1, c)          # sink it in 4 directions
+        sink(r - 1, c)
+        sink(r, c + 1)
+        sink(r, c - 1) 
+
+    for r in range(rows):
+        for c in range(cols):
+            if grid[r][c] == "1":
+                count += 1
+                sink(r, c)      # for nested function, we should keep senk function body before it is called(upper)
+    return count
+
+print(numIsIland([
+   ["1","1","1","1","0"],
+   ["1","1","0","1","0"],
+   ["1","1","0","0","0"],
+   ["0","0","0","0","0"]
+ ]))
